80e13d4fc607ec2e679929d2d21e33f17c100d23
unknown <vasanth889@gmail.com>
2023-06-30 19:51:08 +0530

    wartest files all added

--- demo/demo/src/main/java/com/tutorialspoint/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/tutorialspoint/demo/DemoApplication.java
@@ -2,23 +2,34 @@ package com.tutorialspoint.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @SpringBootApplication
 @RestController
-public class DemoApplication {
-
+public class DemoApplication  extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 	@RequestMapping(value = "/")
 	public String hello() {
-		return "Hello World";
+		return "Hello World from Tomcat";
 	}
+
 	@RequestMapping(value = "/user")
 	public String user() {
-		return "User login";
+		return "user login";
+	}
+
+	@RequestMapping(value = "/working")
+	public String war() {
+		return "war working";
 	}
 }
